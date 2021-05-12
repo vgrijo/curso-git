6ceda01bc7fe60aa6c5dc141704601f7437987af
Victor Guedes <victor.h.grijo@outlook.com>
2021-05-12 15:12:04 -0300

    Acrescentada mensagem de Bons estudos

--- Projeto-Teste/Program.cs
+++ Projeto-Teste/Program.cs
@@ -7,6 +7,7 @@ namespace Projeto_Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bons estudos");
         }
     }
 }
